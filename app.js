9c7bc6fcc270419d02316668fb77c04e3e16c267
Colin Edwards <ColinJhEdwards@gmail.com>
2021-01-15 21:03:51 -0800

    created an array for 15 quiz questions with choices and correct answers

---
+++ app.js
@@ -0,0 +1,119 @@
+const questions = [
+  {
+    question: "1. How do you write 'Hello World' in an alert box?",
+    choices: [
+      "msg('Hello World')",
+      "msgBox('Hello World');",
+      "alertBox('Hello World');",
+      "alert('Hello World');",
+    ],
+    correctAnswer: 3,
+  },
+  {
+    question: "2. Inside which HTML element do you put the JavaScript?",
+    choices: ["<javascript>", "<link>", "<script", "<style>"],
+    correctAnswer: 2,
+  },
+  {
+    question: "3. What is the correct location to insert a Javascript tag?",
+    choices: [
+      "The <head> section",
+      "The <body> section",
+      "The <style> section",
+      "Both the <head> and <body> section will work",
+    ],
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "4. What is the correct syntax for referring to an external script called 'app.js'?",
+    choices: [
+      "<script href='app.js'>",
+      "<script src='app.js'>",
+      "<script link='app.js'>",
+      "<script name='app.js'>",
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question: "5. How do you call a function named'setInterval'?",
+    choices: [
+      "setInterval()",
+      "call setInterval()",
+      "call function setInterval()",
+      "run setInterval()",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question: "6. How do you write an 'if' statement in javascript?",
+    choices: ["if i = 5", "if i === 5", "if (i == 5)", "if i = 5 then"],
+    correctAnswer: 2,
+  },
+  {
+    question: "7. What does CSS stand for?",
+    choices: [
+      "Cascading Style Sheet",
+      "Computer Style Sheets",
+      "Creative Style Sheets",
+      "Condensed Style Sheets",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question: "8. Which is the correct CSS syntax?",
+    choices: [
+      "body:color=black;",
+      "{body:color=black;}",
+      "{body;color:black}",
+      "body{color:black;}",
+    ],
+    correctAnswer: 3,
+  },
+  {
+    question: "9. How do you insert a comment in a CSS file?",
+    choices: [
+      "// this is a comment //",
+      "// this is a comment",
+      "/* this is a comment */",
+      "'this is a comment",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "10. Which property is used to change the background color?",
+    choices: ["background-color", "color", "bgColor", "set-background"],
+    correctAnswer: 0,
+  },
+  {
+    question: "11. Which property is used to change the text size?",
+    choices: ["font-style", "text-style", "font-size", "text-size"],
+    correctAnswer: 2,
+  },
+  {
+    question: "12. How do you select an element with an id of 'demo'?",
+    choices: ["demo", "*demo", ".demo", "#demo"],
+    correctAnswer: 3,
+  },
+  {
+    question: "13. What is the correct HTML element for the largest heading?",
+    choices: ["<h1>", "<heading>", "<h6>", "<headXL>"],
+    correctAnswer: 0,
+  },
+  {
+    question: "14. How can you open a link in a new tab?",
+    choices: [
+      "<a href='url' new>",
+      "<a href='url' target='new'>",
+      "<a href='url' target='_blank'>",
+      "<a href='url' newTarget>",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question:
+      "15. Which HTML attribute specifies an alternate text for an image, if the image cannot be display?",
+    choices: ["description=''", "alt=''", "src=''", "title=''"],
+    correctAnswer: 1,
+  },
+];
